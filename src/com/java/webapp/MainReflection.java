47e4b7be859000b18c16752e2340bbb5a9751fe1
Yura <yura.vashchenko@gmail.com>
2021-01-19 12:59:05 +0200

    HW4 : Fix reflection realization

--- src/com/java/webapp/MainReflection.java
+++ src/com/java/webapp/MainReflection.java
@@ -9,8 +9,8 @@ public class MainReflection {
     public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         Resume resume = new Resume();
         Class classObject = resume.getClass();
-        Method toStringMethod = classObject.getMethod("toString", null);
-        String returnedValue = (String) toStringMethod.invoke(resume);
+        Method toStringMethod = classObject.getMethod("toString");
+        Object returnedValue = toStringMethod.invoke(resume);
         System.out.println(returnedValue);
     }
 }
