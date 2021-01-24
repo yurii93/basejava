35d183a7398df78d710e07d69f9846a9fdf923b2
Yura <yura.vashchenko@gmail.com>
2021-01-24 15:16:05 +0200

    HW8: Add test data class and use it in AbstractStorageTest

---
+++ src/com/java/webapp/ResumeTestData.java
@@ -0,0 +1,35 @@
+package com.java.webapp;
+
+import com.java.webapp.model.*;
+
+import java.time.Month;
+
+public class ResumeTestData {
+
+    public static Resume createTestResume(String uuid, String fullname) {
+        Resume resume = new Resume(uuid, fullname);
+        resume.addContact(ContactType.MAIL, "mail1@ya.ru");
+        resume.addContact(ContactType.PHONE, "11111");
+        resume.addSection(SectionType.OBJECTIVE, new TextSection("Objective1"));
+        resume.addSection(SectionType.PERSONAL, new TextSection("Personal data"));
+        resume.addSection(SectionType.ACHIEVEMENT, new ListSection("Achivment11", "Achivment12", "Achivment13"));
+        resume.addSection(SectionType.QUALIFICATIONS, new ListSection("Java", "SQL", "JavaScript"));
+        resume.addSection(SectionType.EXPERIENCE,
+                new OrganizationSection(
+                        new Organization("Organization11", "http://Organization11.ru",
+                                new Organization.Position(2005, Month.JANUARY, "position1", "content1"),
+                                new Organization.Position(2001, Month.MARCH, 2005, Month.JANUARY, "position2", "content2"))));
+        resume.addSection(SectionType.EDUCATION,
+                new OrganizationSection(
+                        new Organization("Institute", null,
+                                new Organization.Position(1996, Month.JANUARY, 2000, Month.DECEMBER, "aspirant", null),
+                                new Organization.Position(2001, Month.MARCH, 2005, Month.JANUARY, "student", "IT facultet")),
+                        new Organization("Organization12", "http://Organization12.ru")));
+        resume.addSection(SectionType.EXPERIENCE,
+                new OrganizationSection(
+                        new Organization("Organization2", "http://Organization2.ru",
+                                new Organization.Position(2015, Month.JANUARY, "position1", "content1"))));
+
+        return resume;
+    }
+}
--- test/com/java/webapp/storage/AbstractStorageTest.java
+++ test/com/java/webapp/storage/AbstractStorageTest.java
@@ -1,17 +1,17 @@
 package com.java.webapp.storage;
 
+import com.java.webapp.ResumeTestData;
 import com.java.webapp.exception.ExistStorageException;
 import com.java.webapp.exception.NotExistStorageException;
-import com.java.webapp.model.*;
+import com.java.webapp.model.Resume;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertSame;
 
 public abstract class AbstractStorageTest {
     protected Storage storage;
@@ -27,36 +27,17 @@ public abstract class AbstractStorageTest {
     private static final Resume R4;
 
     static {
-        R1 = new Resume(UUID_1, "Name1");
-        R2 = new Resume(UUID_2, "Name2");
-        R3 = new Resume(UUID_3, "Name3");
-        R4 = new Resume(UUID_4, "Name4");
+        R1 = ResumeTestData.createTestResume(UUID_1, "Alex Sidorov");
+        R2 = ResumeTestData.createTestResume(UUID_2, "Ivan Petrov");
+        R3 = ResumeTestData.createTestResume(UUID_3, "Vasyl Gigienko");
+        R4 = ResumeTestData.createTestResume(UUID_4, "Yurii Grechko");
 
-        R1.addContact(ContactType.MAIL, "mail1@ya.ru");
-        R1.addContact(ContactType.PHONE, "11111");
-        R1.addSection(SectionType.OBJECTIVE, new TextSection("Objective1"));
-        R1.addSection(SectionType.PERSONAL, new TextSection("Personal data"));
-        R1.addSection(SectionType.ACHIEVEMENT, new ListSection("Achivment11", "Achivment12", "Achivment13"));
-        R1.addSection(SectionType.QUALIFICATIONS, new ListSection("Java", "SQL", "JavaScript"));
-        R1.addSection(SectionType.EXPERIENCE,
-                new OrganizationSection(
-                        new Organization("Organization11", "http://Organization11.ru",
-                                new Organization.Position(2005, Month.JANUARY, "position1", "content1"),
-                                new Organization.Position(2001, Month.MARCH, 2005, Month.JANUARY, "position2", "content2"))));
-        R1.addSection(SectionType.EDUCATION,
-                new OrganizationSection(
-                        new Organization("Institute", null,
-                                new Organization.Position(1996, Month.JANUARY, 2000, Month.DECEMBER, "aspirant", null),
-                                new Organization.Position(2001, Month.MARCH, 2005, Month.JANUARY, "student", "IT facultet")),
-                        new Organization("Organization12", "http://Organization12.ru")));
-        R2.addContact(ContactType.SKYPE, "skype2");
-        R2.addContact(ContactType.PHONE, "22222");
-        R1.addSection(SectionType.EXPERIENCE,
-                new OrganizationSection(
-                        new Organization("Organization2", "http://Organization2.ru",
-                                new Organization.Position(2015, Month.JANUARY, "position1", "content1"))));
+        /*R1 = new Resume(UUID_1, "Ivan Ivanov");
+        R2 = new Resume(UUID_2, "Petro Sidorov");
+        R3 = new Resume(UUID_3, "Petya Nikulin");
+        R4 = new Resume(UUID_4, "Petrovich");*/
     }
-    
+
     protected AbstractStorageTest(Storage storage) {
         this.storage = storage;
     }
@@ -82,18 +63,18 @@ public abstract class AbstractStorageTest {
 
     @Test
     public void update() {
-        Resume newResume = new Resume(UUID_1, "New Name");
+        Resume newResume = ResumeTestData.createTestResume(UUID_1, "Pavel Sidorovich");
         storage.update(newResume);
-        assertTrue(newResume == storage.get(UUID_1));
+        assertSame(newResume, storage.get(UUID_1));
     }
 
     @Test(expected = NotExistStorageException.class)
-    public void updateNotExist() throws Exception {
+    public void updateNotExist() {
         storage.get("dummy");
     }
 
     @Test
-    public void getAll() {
+    public void getAllSorted() {
         List<Resume> list = storage.getAllSorted();
         assertEquals(3, list.size());
         assertEquals(list, Arrays.asList(R1, R2, R3));
