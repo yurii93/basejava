20da4f85e7afe892c772b87cc765e948d758f3f1
Yura <yura.vashchenko@gmail.com>
2021-02-03 15:54:03 +0200

    Lesson 10: fix Json serialization

---
+++ test/com/java/webapp/storage/JsonPathStorageTest.java
@@ -0,0 +1,10 @@
+package com.java.webapp.storage;
+
+import com.java.webapp.storage.serializer.JsonStreamSerializer;
+
+public class JsonPathStorageTest extends AbstractStorageTest {
+
+    public JsonPathStorageTest() {
+        super(new PathStorage(STORAGE_DIR.getAbsolutePath(), new JsonStreamSerializer()));
+    }
+}
