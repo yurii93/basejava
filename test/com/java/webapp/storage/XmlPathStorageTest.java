c2639644688eac427aed541e42c0627610710334
Yura <yura.vashchenko@gmail.com>
2021-02-03 15:53:29 +0200

    Lesson 10: fix Xml serialization

---
+++ test/com/java/webapp/storage/XmlPathStorageTest.java
@@ -0,0 +1,10 @@
+package com.java.webapp.storage;
+
+import com.java.webapp.storage.serializer.XmlStreamSerializer;
+
+public class XmlPathStorageTest extends AbstractStorageTest {
+
+    public XmlPathStorageTest() {
+        super(new PathStorage(STORAGE_DIR.getAbsolutePath(), new XmlStreamSerializer()));
+    }
+}
